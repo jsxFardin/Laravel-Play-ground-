56d711eebd447500e5299688f3e3965f5045ef4a
jsxfardin <fardin.ahsan75@gmail.com>
2021-11-17 03:16:49 +0600

    video chat

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,24 @@ use Illuminate\Support\Facades\Route;
 use Stichoza\GoogleTranslate\GoogleTranslate;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/translate', function () {
 
     return GoogleTranslate::trans('i love u', 'bn', 'en');
-    return view('welcome');
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['middleware' => 'auth'], function(){
+
+    Route::get('/video_chat', [App\Http\Controllers\VideoChatController::class, 'index'])->name('video_chat');
+    Route::post('auth/video_chat', [App\Http\Controllers\VideoChatController::class, 'auth'])->name('auth_video_chat');
+    // Route::get('video_chat', 'VideoChatController@index');
+    // Route::post('auth/video_chat', 'VideoChatController@auth');
 });
